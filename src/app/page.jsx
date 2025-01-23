c3014dd2e3e64671a1711a6502f70542116cc772
Chung Nguyen <zrmedia9@gmail.com>
2025-01-23 21:46:01 +0700

    Add post listing and detail view components with database integration

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,23 @@
-export default function Home() {
+import PostCard from "@/components/PostCard";
+import { getCollection } from "@/lib/db";
+
+export default async function Home() {
+  const postsCollection = await getCollection("posts");
+  const posts = await postsCollection?.find().sort({ $natural: -1 }).toArray();
+
+  if (!posts) {
+    return (
+      <p>Failing to load posts</p>
+    );
+  }
 
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="grid grid-cols-2 gap-6">
+      {posts.map((post) => (
+        <div key={post._id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   );
 }
